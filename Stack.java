c210ed254909426dff04fa81198494524e457e88
rravisha069 <40995920+rravisha069@users.noreply.github.com>
2022-04-04 22:10:49 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,13 +1,19 @@
-public class Stack{
-  int nums[] = new int[5];
+public class StackUsingArray {
+  int stack[] = new int[5];
   int top = 0;
 
-  public void push(int num) {
-    nums[top] = num;
+  public void push(int data) {
+    stack[top] = data;
     top++;
   }
 
   public void pop() {
-    
+    stack[top-1] = 0;
+  }
+
+  public int peek() {
+    int data = 0;
+    data = stack[top-1];
+    return data;
   }
 }
