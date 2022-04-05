224656cd7039ee3ae11e4667eee109cb88372387
rravisha069 <40995920+rravisha069@users.noreply.github.com>
2022-04-05 11:08:54 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,32 +1,44 @@
-public class Queue {
-  int capacity = 4;
+public class QueueUsingArray {
+  int rear=0;
+  int front=0;
+  int capacity = 5;
   int queue[] = new int[capacity];
-  int front = 0;
-  int rear = 0;
-  int size = 0;
+  int size=0;
 
   public void enqueue(int data) {
-    if(size<capacity){
+    if(isFull()) {
+      System.out.println("queue overflow");
+    } else {
       queue[rear] = data;
-      rear = (rear+1)%capacity;
-      size++;
+      rear = (rear + 1) % capacity;
+      size = size + 1;
     }
-  } 
+  }
 
   public int dequeue() {
     int data = queue[front];
-    if(size!=0) {
-      front = (front+1)%capacity;
-      size--;
-    } else {
-      System.out.println("Queue is Empty");
+    if(!isEmpty()) {
+      front = (front + 1) % capacity;
+      size = size - 1;
     }
     return data;
   }
 
+  public int getSize() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return getSize() == 0;
+  }
+
+  public boolean isFull() {
+    return getSize() == capacity;
+  }
+
   public void show() {
-    for(int i=0;i<size; i++){
-      System.out.println(queue[(front+i)%capacity] + " ");
+    for(int i=0; i<size; i++) {
+      System.out.println(queue[(front + i)%capacity]);
     }
   }
 }
